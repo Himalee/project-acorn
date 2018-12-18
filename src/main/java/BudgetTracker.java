ce612a29f6582388ce2bd192d0005399f8027a29
Himalee <himalee.tailor@gmail.com>
2018-12-18 11:32:47 +0000

    Update: user flow in BudgetTracker

--- src/main/java/BudgetTracker.java
+++ src/main/java/BudgetTracker.java
@@ -1,9 +1,31 @@
 public class BudgetTracker {
 
+    public Display display;
+
+    public BudgetTracker(Display display) {
+        this.display = display;
+    }
+
     public void start() {
+        welcomeUser();
+        displayMenu();
+        userMenuChoice();
+        goodbye();
+    }
 
-        Display display = new Display();
+    private void welcomeUser() {
         display.present("Welcome to Project Acorn");
+    }
 
+    private void displayMenu() {
+        display.present("Menu:\n1. Quit\nPlease enter your choice:");
+    }
+
+    private void userMenuChoice() {
+        display.receiveString();
+    }
+
+    private void goodbye() {
+        display.present("Goodbye");
     }
 }
